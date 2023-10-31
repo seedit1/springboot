7f57f2c0418c6fed6a0d240ef653c649771acbb6
seedit1 <woom1441@naver.com>
2023-10-31 13:54:06 +0900

    get + push

--- src/main/java/com/cos/blog/test/HttpControllerTest.java
+++ src/main/java/com/cos/blog/test/HttpControllerTest.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 //사용자가 요청 -> 응답(HTML 파일)
@@ -18,14 +19,14 @@ public class HttpControllerTest {
 	//인터넷브라우저요청은 무조건 get요청만 할수 있다. 
 	//http://localhost:8080/http/get(select)
 	@GetMapping("/http/get")
-	public String getTest() {
-		return "get 요청";
+	public String getTest(Member m) { //id=1&username=1234&password=1234&email=ssar@nate.com
+		return "get 요청 :" + m.getId() + ", " +m.getUsername() + "," + m.getPassword()+ ", " + m.getEmail();
 	}
 	
 	//http://localhost:8080/http/post(insert)
 	@PostMapping("/http/post")
-	public String postTest() {
-		return "post 요청";
+	public String postTest(Member m) {
+		return "post 요청 :"+ m.getId() + ", " +m.getUsername() + "," + m.getPassword()+ ", " + m.getEmail();
 	}
 	
 	//http://localhost:8080/http/put(update)
