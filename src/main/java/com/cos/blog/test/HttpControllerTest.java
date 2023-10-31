94fd30d4580d3715432a453c620d22ef294fe7e2
seedit1 <woom1441@naver.com>
2023-10-31 14:32:28 +0900

    put

--- src/main/java/com/cos/blog/test/HttpControllerTest.java
+++ src/main/java/com/cos/blog/test/HttpControllerTest.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,15 +25,15 @@ public class HttpControllerTest {
 	}
 	
 	//http://localhost:8080/http/post(insert)
-	@PostMapping("/http/post")
-	public String postTest(Member m) {
+	@PostMapping("/http/post") // text/plain , application/json 
+	public String postTest(@RequestBody Member m) { //MessageConverter(스프링부트)
 		return "post 요청 :"+ m.getId() + ", " +m.getUsername() + "," + m.getPassword()+ ", " + m.getEmail();
 	}
 	
 	//http://localhost:8080/http/put(update)
 	@PutMapping("/http/put")
-	public String putTest() {
-		return "put 요청";
+	public String putTest(@RequestBody Member m) {
+		return "put 요청 : " + m.getId() + ", " +m.getUsername() + "," + m.getPassword()+ ", " + m.getEmail();
 	}
 	
 	//http://localhost:8080/http/delete(delete)
